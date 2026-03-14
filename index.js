a597511642d930af6809ef5bbcfb736e8f5e3d59
Hryhorii <grigorijsilin@gmail.com>
2026-03-14 18:12:15 +0200

    Fixed bug with footer and humburger button

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 window.onload = function () {
     var button = document.getElementById('toggle-menu');
+    var menu = document.getElementById('main-menu');
 
     button.addEventListener('click', function (event) {
         event.preventDefault();
-        var menu = document.getElementById('main-menu');
         menu.classList.toggle('is-open');
+    });
+
+    var links = menu.querySelectorAll('a');
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function() {
+            menu.classList.remove('is-open');
+        });
     });
 };
